17f9ded7e78f7c71c02c04551703a1bf31b0168b
Yudit2 <99101174+Yudit2@users.noreply.github.com>
2022-04-24 17:18:33 -0600

    Add files via upload

---
+++ backend/Actualizar.py
@@ -0,0 +1,22 @@
+class actualizar:
+    def __init__(self,id_libro,tipo_libro,autor,contador, disponible, no_disponible,año,editorial):
+        self.persona =persona
+        
+    def getPersona(self):
+        return self.persona
+    
+    def setPersona(self,persona):
+        self.persona = persona
+
+
+
+    
+
+
+
+
+
+
+
+
+
